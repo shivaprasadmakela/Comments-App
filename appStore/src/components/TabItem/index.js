ddd246e7a7f1b643629c91dda05d9cd0dc4c98e3
shivaprasad <shivaprasadmakela@gmail.com>
2023-04-26 10:25:29 +0000

    first commit

---
+++ appStore/src/components/TabItem/index.js
@@ -0,0 +1,23 @@
+// Write your code here
+import './index.css'
+
+const TabItem = props => {
+  const {tabDetails, selectedTabId, isActive} = props
+  const {tabId, displayText} = tabDetails
+
+  const onClickTab = () => {
+    selectedTabId(tabId)
+  }
+
+  const tabClassName = isActive ? 'tab-button active' : 'tab-button'
+
+  return (
+    <li className="tab-item">
+      <button className={tabClassName} onClick={onClickTab} type="button">
+        {displayText}
+      </button>
+    </li>
+  )
+}
+
+export default TabItem
